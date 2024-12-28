cbc537256bf01585630607688341c3234ba2f234
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-12-28 17:43:50 +0530

    Create app.component.ts

---
+++ CurrencyPipe/app.component.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { HighLightDirective } from './high-light.directive';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+ 
+  title="Welcome to Angular 18"
+  price=200
+
+  
+}
+
