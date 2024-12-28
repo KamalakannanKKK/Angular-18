0ac6aa3a77ebf43ef9eb9f03a1bbf0830d2ab61f
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-12-28 17:27:19 +0530

    Create app.component.ts

---
+++ DatePipe/app.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { HighLightDirective } from './high-light.directive';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+ 
+  title="Angular 18"
+
+  toDate=new Date();
+}
