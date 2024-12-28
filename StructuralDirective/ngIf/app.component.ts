5377cadf70c692d21809b79c142e1d45903edc68
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-12-28 17:14:09 +0530

    Create app.component.ts

---
+++ StructuralDirective/ngIf/app.component.ts
@@ -0,0 +1,27 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { HighLightDirective } from './high-light.directive';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,FormsModule,CommonModule,HighLightDirective],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Sachin Tendulkar';
+  checkBoolean=true;
+
+  movies:String[] =[
+     "Zootopia","Amaran","I"
+  ]
+
+  clickCount:number=0;
+
+  onSave(){
+    this.clickCount=this.clickCount+1;
+  }
+  
+}
