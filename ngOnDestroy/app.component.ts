46e70d5db617987519faeb47c7e8db1a6d66b8b4
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-12-29 16:04:03 +0530

    Create app.component.ts

---
+++ ngOnDestroy/app.component.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { HighLightDirective } from './high-light.directive';
+import { FormComponent } from './form/form.component';
+import { CarListComponent } from './car-list/car-list.component';
+import { ChildComponenetComponent } from './child-componenet/child-componenet.component';
+import { SimpleMessageComponent } from './simple-message/simple-message.component';
+import { HelloComponent } from './hello/hello.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet,CommonModule,FormsModule,HelloComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent{
+ 
+    title="Welcome to Angular 18"
+    showTrue=''
+  
+}
